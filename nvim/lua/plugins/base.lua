fc379e175020a342e10190a0460e4f03444010fd
Daniel Schneider <dschneider0103@gmail.com>
2023-09-12 23:22:06 +0200

    remove markdown preview

--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -70,14 +70,6 @@ return {
 	},
   --Markdown
   {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    keys = { {"<leader>m", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview"} },
-    config = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-  {
     'jakewvincent/mkdnflow.nvim',
     config = function()
         require('mkdnflow').setup()
