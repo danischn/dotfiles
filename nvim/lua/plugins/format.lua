fa8f66fce91466b04615a95539ce4260222123fb
Daniel Schneider <dschneider0103@gmail.com>
2023-09-18 23:06:17 +0200

    update

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,11 +1,12 @@
 return {
 	"mhartington/formatter.nvim",
-  event = { "BufNewFile", "BufRead" },
-  config = function()
-    require("formatter").setup({
-      filetype = {
-        lua = { require("formatter.filetypes.lua").stylua },
-        python = { require("formatter.filetypes.python").black },
+	event = { "BufNewFile", "BufRead" },
+	config = function()
+		require("formatter").setup({
+			filetype = {
+				lua = { require("formatter.filetypes.lua").stylua },
+				python = { require("formatter.filetypes.python").black },
+				markdown = { require("formatter.filetypes.markdown").prettier },
 			},
 		})
 	end,
