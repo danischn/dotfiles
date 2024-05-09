855c723a3a0546a2448866fda17e478126f299bb
Daniel Schneider <dschneider0103@gmail.com>
2024-05-09 18:11:53 +0200

    update

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,15 +10,16 @@ map("n", "N", "Nzzzv")
 --Remap space as leader keys
 map("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
+
 -- Clear highlights
 map("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+
 -- Better join
 map("n", "J", "mzJ`z")
+
 -- Better paste
 map("v", "p", "P", opts)
--- Insert --
--- Press jk fast to enter
-map("i", "jk", "<ESC>", opts)
+
 -- Visual --
 -- Stay in indent mode
 map("v", "<", "<gv", opts)
