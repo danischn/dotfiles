d0e11762494d07589e9540ad447688ae36b0445e
Daniel Schneider <dschneider0103@gmail.com>
2024-12-11 14:39:25 +0100

    Inital commit

--- nvim/colors/iben.lua
+++ nvim/colors/iben.lua
@@ -3,270 +3,152 @@ vim.g.colors_name = "iben"
 vim.o.background = "light"
 
 local colors = {
-	base0 = "#1A1918", -- hsv(30, 8%, 10%)
-	base1 = "#595855", -- hsv(45, 4%, 35%)
-	base2 = "#807E79", -- hsv(43, 5%, 50%)
-	base3 = "#999791", -- hsv(45, 5%, 60%)
-	base4 = "#B3B1AD", -- hsv(40, 3%, 70%)
-	base5 = "#CCCBC6", -- hsv(50, 3%, 80%)
-	base6 = "#E6E4DF", -- hsv(43, 3%, 90%)
-	base7 = "#FAF2EB", -- hsv(24, 4%, 98%)
-
-	light_orange = "#FAE1C8", -- hsv(30, 20%, 98%)
-	light_yellow = "#FAFAC8", -- hsv(60, 20%, 98%)
-	light_cyan = "#C8FAFA", -- hsv(180, 20%, 98%)
-	light_green = "#D4FAD4", -- hsv(120, 15%, 98%)
-	light_blue = "#D4D4FA", -- hsv(240, 15%, 98%)
-	light_purple = "#EDD4FA", -- hsv(280, 15%, 98%)
-	light_pink = "#FAD4ED", -- hsv(320, 15%, 98%)
-	light_red = "#FAD4D4", -- hsv(360, 15%, 98%)
-
-	orange = "#F27900", -- hsv(30, 100%, 95%)
-	yellow = "#F2F200", -- hsv(60, 100%, 95%)
-	cyan = "#00A6A6", -- hsv(180, 100%, 65%)
-	green = "#00A600", -- hsv(120, 100%, 65%)
-	blue = "#0000A6", -- hsv(240, 100%, 65%)
-	purple = "#6F00A6", -- hsv(280, 100%, 65%)
-	pink = "#A6006F", -- hsv(320, 100%, 65%)
-	red = "#A60000", -- hsv(360, 100%, 65%)
+	foreground = "#000000",
+	background = "#FAF2EB",
+	highlight = "#FAFAFA",
+	critical = "#FF6F00",
+	popout = "#FFAB91",
+	strong = { fg = "#000000", bold = true },
+	default = "#000000",
+	salient = "#673AB7",
+	subtle = "#ECEFF1",
+	faded = "#B0BEC5",
+	search = "#FFFF00",
 }
 
 local hl_groups = {
-	Normal = { fg = colors.base0, bg = colors.base7 },
+	-- General Appearance
+	Normal = { fg = colors.foreground, bg = colors.background },
 	NormalFloat = { link = "Normal" },
-	NonText = { fg = colors.base3 },
-
-	SpecialKey = { fg = colors.blue },
-	Directory = { link = "SpecialKey" },
-
-	Title = { fg = colors.base0, bold = true },
-
-	IncSearch = { fg = colors.base0, bg = colors.light_yellow },
-	Search = { link = "IncSearch" },
-	CurSearch = { link = "IncSearch" },
-
-	LineNr = { fg = colors.base3 },
-	CursorLineNr = { link = "Normal" },
-
-	Question = { fg = colors.base0, bold = true },
-
-	StatusLine = { fg = colors.base0, bg = colors.base7, bold = true },
-	StatusLineNC = { fg = colors.base0, bg = colors.base7 },
-
+	CursorLine = { bg = colors.subtle },
+	CursorColumn = { link = "CursorLine" },
+	ColorColumn = { bg = colors.highlight },
 	TabLine = { link = "Normal" },
 	TabLineSel = { reverse = true },
+	StatusLine = { fg = colors.foreground, bg = colors.highlight, bold = true },
+	StatusLineNC = { fg = colors.foreground, bg = colors.highlight },
 
-	WinSeparator = { fg = colors.base0 },
-
+	-- Line Numbering
+	LineNr = { fg = colors.faded },
+	CursorLineNr = { link = "Normal" },
 	SignColumn = { link = "LineNr" },
 	FoldColumn = { link = "SignColumn" },
 
-	Conceal = { fg = colors.base5 },
-	SpellBad = { fg = colors.base0, undercurl = true },
+	-- Menus and Selection
+	Pmenu = { fg = colors.foreground, bg = colors.highlight },
+	PmenuSel = { fg = colors.foreground, bg = colors.faded },
+	PmenuSbar = { bg = colors.foreground },
+	PmenuThumb = { link = "PmenuSbar" },
+	WildMenu = { bg = colors.highlight },
+
+	-- Search and Matching
+	IncSearch = { fg = colors.default, bg = colors.search },
+	Search = { link = "IncSearch" },
+	CurSearch = { link = "IncSearch" },
+	MatchParen = { fg = colors.critical, underline = true },
+
+	-- Diagnostic & Messaging
+	Question = { fg = colors.foreground, bold = true },
+	MoreMsg = { fg = colors.salient },
+	ModeMsg = { fg = colors.salient },
+	ErrorMsg = { fg = colors.background, bg = colors.critical },
+	WarningMsg = { fg = colors.background, bg = colors.critical },
+
+	-- Spell Checks
+	SpellBad = { fg = colors.foreground, undercurl = true },
 	SpellCap = { link = "SpellBad" },
 	SpellLocal = { link = "SpellBad" },
 	SpellRare = { link = "SpellBad" },
 
-	Pmenu = { fg = colors.base0, bg = colors.base7 },
-	PmenuSel = { fg = colors.base0, bg = colors.base3 },
-	PmenuSbar = { bg = colors.base0 },
-	PmenuThumb = { link = "PmenuSbar" },
-
-	WildMenu = { bg = colors.base4 },
-	Visual = { bg = colors.base5 },
-	Folded = {},
-
-	Cursor = { bg = colors.base2 },
-	TermCursor = { link = "Cursor" },
-	CursorLine = { bg = colors.base6 },
-	CursorColumn = { link = "CursorLine" },
-
-	ColorColumn = { bg = colors.light_yellow },
-
-	MoreMsg = { fg = colors.cyan },
-	ModeMsg = { fg = colors.blue },
-	ErrorMsg = { fg = colors.base7, bg = colors.red },
-	WarningMsg = { fg = colors.base7, bg = colors.orange },
-
-	DiffAdd = { fg = colors.green },
-	DiffChange = { fg = colors.orange },
-	DiffDelete = { fg = colors.red },
-
-	Comment = { fg = colors.base3 },
-	Constant = { fg = colors.base0, bg = colors.light_blue }, -- String Character Number Boolean Float
-	String = { fg = colors.base0, bg = colors.light_green },
-	Identifier = { fg = colors.base0 },
+	-- Text Highlights
+	Comment = { fg = colors.faded },
+	Constant = { fg = colors.salient },
+	String = { fg = colors.popout },
+	Identifier = { fg = colors.default },
 	Delimiter = { link = "Identifier" },
-	Statement = { fg = colors.base0, bg = colors.light_orange }, -- Conditional Repeat Label Operator Keyword Exception
+	Statement = { fg = colors.salient },
+	Function = colors.strong,
 	Operator = { link = "Identifier" },
-	PreProc = { link = "Question" }, -- Include Define Macro PreCondit
-	Type = { fg = colors.pink }, -- StorageClass Structure Typedef
-	Special = { link = "SpecialKey" }, -- SpecialChar Tag Delimiter SpecialComment Debug
-	Function = { fg = colors.base0, bold = true },
+	PreProc = { link = "Question" },
+	Type = { fg = colors.default },
+	Special = { link = "SpecialKey" },
 	Underlined = { underline = true },
-	Ignore = { fg = colors.base7 },
+	Ignore = { fg = colors.background },
 	Error = { link = "ErrorMsg" },
-	Todo = { fg = colors.green },
+	Todo = { fg = colors.salient },
 
-	MatchParen = { fg = colors.red, underline = true },
+	-- Miscellaneous
+	SpecialKey = { fg = colors.foreground },
+	Directory = { link = "SpecialKey" },
+	NonText = { fg = colors.faded },
+	Folded = {},
+	Cursor = { bg = colors.faded },
+	TermCursor = { link = "Cursor" },
+	Visual = { bg = colors.faded },
+	DiffAdd = { fg = colors.foreground, bg = colors.salient },
+	DiffChange = { fg = colors.foreground, bg = colors.popout },
+	DiffDelete = { fg = colors.foreground, bg = colors.critical },
 
 	-- Plugins
 
 	-- Treesitter
 	["@variable"] = { link = "Identifier" },
-	["@variable.builtin"] = { link = "Type" },
-	["@variable.parameter"] = { link = "Identifier" },
-	["@variable.member"] = { link = "Identifier" },
-
 	["@constant"] = { link = "Constant" },
-	["@constant.builtin"] = { link = "Type" },
-	["@constant.macro"] = { link = "Define" },
-
 	["@module"] = { link = "Include" },
 	["@module.builtin"] = { link = "Include" },
 	["@label"] = { link = "Label" },
-
 	["@string"] = { link = "String" },
-	["@string.documentation"] = { link = "String" },
-	["@string.regexp"] = { link = "String" },
-	["@string.escape"] = { link = "SpecialChar" },
-	["@string.special"] = { link = "SpecialChar" },
-	["@string.special.symbol"] = { link = "Identifier" },
-	["@string.special.path"] = { link = "Underlined" },
-	["@string.special.url"] = { link = "Underlined" },
-
 	["@character"] = { link = "Character" },
 	["@character.special"] = { link = "SpecialChar" },
-
 	["@boolean"] = { link = "Boolean" },
 	["@number"] = { link = "Number" },
 	["@number.float"] = { link = "Float" },
-
 	["@type"] = { link = "Type" },
 	["@type.builtin"] = { link = "Type" },
-	["@type.qualifier"] = { link = "Type" },
-	["@type.definition"] = { link = "Typedef" },
-
 	["@attribute"] = { link = "PreProc" },
 	["@property"] = { link = "Identifier" },
-
 	["@function"] = { link = "Function" },
-	["@function.builtin"] = { link = "Special" },
-	["@function.call"] = { link = "Special" },
-	["@function.macro"] = { link = "Macro" },
-
-	["@function.method"] = { link = "@function" },
-	["@function.method.call"] = { link = "@function.call" },
-
+	["@function.builtin"] = { link = "Function" },
 	["@constructor"] = { link = "Identifier" },
 	["@operator"] = { link = "Operator" },
-
 	["@keyword"] = { link = "Keyword" },
-	["@keyword.coroutine"] = { link = "Keyword" },
-	["@keyword.function"] = { bg = colors.light_purple },
-	["@keyword.operator"] = { link = "Keyword" },
-	["@keyword.import"] = { link = "Include" },
-	["@keyword.storage"] = { link = "Keyword" },
-	["@keyword.repeat"] = { link = "Repeat" },
-	["@keyword.return"] = { link = "Keyword" },
-	["@keyword.debug"] = { link = "Debug" },
-	["@keyword.exception"] = { link = "Exception" },
-
-	["@keyword.conditional"] = { link = "Conditional" },
-	["@keyword.conditional.ternary"] = { link = "Conditional" },
-	["@keyword.directive"] = { link = "PreProc" },
-	["@keyword.directive.define"] = { link = "PreProc" },
-
 	["@punctutation.delimiter"] = { link = "Delimiter" },
 	["@punctutation.bracket"] = { link = "Delimiter" },
 	["@punctutation.special"] = { link = "Delimiter" },
-
 	["@comment"] = { link = "Comment" },
-	["@comment.documentation"] = { link = "Comment" },
-
-	["@comment.warning"] = { link = "WarningMsg" },
-	["@comment.error"] = { link = "Error" },
-	["@comment.todo"] = { link = "Todo" },
-	["@comment.note"] = { link = "SpecialComment" },
-
 	["@markup.strong"] = { bold = true },
 	["@markup.emphasis"] = { italic = true },
 	["@markup.underline"] = { underline = true },
 	["@markup.strike"] = { strikethrough = true },
-
 	["@markup.heading"] = { link = "Title" },
 	["@markup.heading.gitcommit"] = { link = "@spell" },
-
 	["@markup.quote"] = { link = "Comment" },
 	["@markup.math"] = { link = "Special" },
 	["@markup.environment"] = { link = "Macro" },
-
 	["@markup.link"] = { link = "Underlined" },
 	["@markup.link.label"] = { link = "SpecialChar" },
 	["@markup.link.url"] = { link = "Keyword" },
-
 	["@markup.raw"] = { link = "SpecialComment" },
 	["@markup.raw.block"] = { link = "SpecialComment" },
-
 	["@markup.list"] = { link = "Identifier" },
 	["@markup.list.checked"] = { link = "Comment" },
 	["@markup.list.unchecked"] = { link = "Identifier" },
-
 	["@tag"] = { link = "Label" },
 	["@tag.delimiter"] = { link = "Delimiter" },
 	["@tag.attribute"] = { link = "Identifier" },
-
 	["@diff.plus"] = { link = "DiffAdd" },
 	["@diff.minus"] = { link = "DiffDelete" },
 	["@diff.delta"] = { link = "DiffChange" },
 
-	-- lsp
-	DiagnosticError = { fg = colors.red },
-	DiagnosticHint = { fg = colors.base3 },
-	DiagnosticInfo = { fg = colors.blue },
-	DiagnosticOk = { fg = colors.green },
-	DiagnosticUnderlineError = { sp = colors.red, undercurl = true },
-	DiagnosticUnderlineHint = { sp = colors.base3, undercurl = true },
-	DiagnosticUnderlineInfo = { sp = colors.blue, undercurl = true },
-	DiagnosticUnderlineOk = { sp = colors.green, undercurl = true },
-	DiagnosticUnderlineWarn = { sp = colors.orange, undercurl = true },
-	DiagnosticWarn = { fg = colors.orange },
-
-	-- lsp highlights
-	["@lsp.type.class"] = { link = "@type" },
-	["@lsp.type.decorator"] = { link = "@function" },
-	["@lsp.type.enum"] = { link = "@type" },
-	["@lsp.type.enumMember"] = { link = "@constant" },
-	["@lsp.type.function"] = { link = "@function.call" },
-	["@lsp.type.interface"] = { link = "@type" },
-	["@lsp.type.macro"] = { link = "@macro" },
-	["@lsp.type.method"] = { link = "@function.call" },
-	["@lsp.type.namespace"] = { link = "@namespace" },
-	["@lsp.type.parameter"] = { link = "@parameter" },
-	["@lsp.type.property"] = { link = "@property" },
-	["@lsp.type.struct"] = { link = "@structure" },
-	["@lsp.type.type"] = { link = "@type" },
-	["@lsp.type.variable"] = { link = "@variable" },
-	["@lsp.typemod.class.defaultLibrary"] = { link = "@type.builtin" },
-	["@lsp.typemod.enum.defaultLibrary"] = { link = "@type.builtin" },
-	["@lsp.typemod.enumMember.defaultLibrary"] = { link = "@constant.builtin" },
-	["@lsp.typemod.function.declaration"] = { link = "@function" },
-	["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
-	["@lsp.typemod.keyword.async"] = { link = "@keyword.coroutine" },
-	["@lsp.typemod.macro.defaultLibrary"] = { link = "@function.builtin" },
-	["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
-	["@lsp.typemod.method.declaration"] = { link = "@function" },
-	["@lsp.typemod.operator.injected"] = { link = "@operator" },
-	["@lsp.typemod.string.injected"] = { link = "@string" },
-	["@lsp.typemod.type.defaultLibrary"] = { link = "@type.builtin" },
-	["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
-	["@lsp.typemod.variable.injected"] = { link = "@variable" },
-
+	-- lsp (more to fill in here)
+	DiagnosticError = { fg = colors.critical },
+	DiagnosticUnderlineError = { sp = colors.critical, undercurl = true },
 	-- nvim-cmp
 	CmpItemAbbr = { link = "Comment" },
 	CmpItemAbbrDeprecated = { link = "Visual" },
 	CmpItemAbbrMatch = { link = "Identifier" },
 	CmpItemAbbrMatchFuzzy = { link = "CmpItemAbbrMatch" },
+
 	CmpItemKindText = { fg = colors.base0 },
 	CmpItemKindSnippet = { fg = colors.base2 },
 	CmpItemKindConstant = { fg = colors.blue },
