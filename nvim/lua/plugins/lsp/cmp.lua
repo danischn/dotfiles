fa748bb68307f95492696dd1f1ae2ff7621afe36
Daniel Schneider <dschneider0103@gmail.com>
2024-12-11 14:08:04 +0100

    finished

--- nvim/lua/plugins/lsp/cmp.lua
+++ nvim/lua/plugins/lsp/cmp.lua
@@ -8,7 +8,7 @@ return {
 		keymap = {
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide" },
-			["<C-o>"] = { "select_and_accept" },
+			["<CR>"] = { "select_and_accept", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback" },
 			["<C-j>"] = { "select_next", "fallback" },
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
@@ -16,29 +16,9 @@ return {
 			["<Tab>"] = { "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
 		},
-		menu = {
-			enabled = true,
-			min_width = 15,
-			max_height = 10,
-			border = "none",
-			winblend = 0,
-			winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
-			scrolloff = 2,
-			scrollbar = true,
-			direction_priority = { "s", "n" },
-			auto_show = true,
-			draw = {
-				align_to_component = "label", -- or 'none' to disable
-				padding = 1,
-				gap = 1,
-				treesitter = false,
-				columns = { { "label", "label_description", gap = 1 },{ "kind_icon" } },
-			},
-		},
 		completion = {
-			list = {
-				max_items = 4,
-			},
+			list = { max_items = 4},
+			menu = { draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon" }, { "kind" } } } },
 		},
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
