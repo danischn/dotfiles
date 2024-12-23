cfd8fc7e104dca066cf03aadaddca693fd626d33
Daniel Schneider <dschneider0103@gmail.com>
2024-12-24 00:40:13 +0100

    add pyright

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,5 @@ require("keymaps")
 require("autocmds")
 require("Lazy")
 
-vim.lsp.enable("luals") -- https://github.com/neovim/neovim/issues/31641
+vim.lsp.enable("luals")
+vim.lsp.enable("pyright")
