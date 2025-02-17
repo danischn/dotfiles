0c95e878d92538db1f19ec4e5cb5ee12507161d1
Daniel Schneider <dschneider0103@gmail.com>
2025-02-17 15:14:01 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,16 +4,18 @@ require("autocmds")
 require("statusline")
 
 require("paq")({
-  "savq/paq-nvim",
+  "savq/paq-nvim", -- Let Paq manage itself
   "kylechui/nvim-surround",
   "williamboman/mason.nvim",
   "aserowy/tmux.nvim",
+  "folke/zen-mode.nvim",
+  -- Vim plugins:
   "preservim/vimux",
   "jpalardy/vim-slime",
-  "folke/zen-mode.nvim",
 })
 
 require("nvim-surround").setup()
 require("mason").setup()
-require("tmux").setup({copy_sync = false})
-require("zen-mode").setup()
+require("tmux").setup({ copy_sync = false })
+require("zen-mode").setup({ window = { width = 100 }, plugins = { tmux = { enabled = true } } })
+vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>")
