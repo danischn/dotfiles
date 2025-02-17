7c6a1b72ec0bec44546dff39801fdd9d4aafa487
Daniel Schneider <dschneider0103@gmail.com>
2025-02-17 18:30:59 +0100

    update

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -4,19 +4,3 @@ vim.keymap.set("n", "<leader><cr>", function()
   local command = "clear; python3 " .. filepath
   vim.cmd(string.format("call VimuxRunCommand('%s')", command))
 end, { buffer = 0, desc = "Run python file" })
-
-vim.lsp.start({
-  cmd = { "basedpyright-langserver", "--stdio" },
-  filetypes = { "python" },
-  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json" },
-  single_file_support = true,
-  settings = {
-    basedpyright = {
-      analysis = {
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "openFilesOnly",
-      },
-    },
-  },
-})
