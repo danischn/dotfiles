24c75e8a2d8e7b9b070b41b7385a92452af675ef
Daniel Schneider <dschneider0103@gmail.com>
2025-02-17 15:57:48 +0100

    update

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,15 @@
+vim.lsp.start({
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+})
