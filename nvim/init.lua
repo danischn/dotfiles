877494c4766d983a646810ccd0a3801bc76ee540
Daniel Schneider <dschneider0103@gmail.com>
2025-02-17 15:25:41 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ require("options")
 require("keymaps")
 require("autocmds")
 require("statusline")
+require("fzf")
 
 require("paq")({
   "savq/paq-nvim", -- Let Paq manage itself
@@ -9,6 +10,8 @@ require("paq")({
   "williamboman/mason.nvim",
   "aserowy/tmux.nvim",
   "folke/zen-mode.nvim",
+  "ibhagwan/fzf-lua",
+  "stevearc/conform.nvim",
   -- Vim plugins:
   "preservim/vimux",
   "jpalardy/vim-slime",
@@ -17,5 +20,16 @@ require("paq")({
 require("nvim-surround").setup()
 require("mason").setup()
 require("tmux").setup({ copy_sync = false })
+
 require("zen-mode").setup({ window = { width = 100 }, plugins = { tmux = { enabled = true } } })
 vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>")
+
+require("conform").setup({
+  formatters_by_ft = {
+    python = { "black" },
+    lua = { "stylua" },
+    markdown = { "prettier" },
+    c = { "clang-format" },
+  },
+})
+vim.keymap.set("n", "<leader>f", "<cmd>lua require('conform').format()<cr>")
