e785f3cd2485932a2867eec1540cd2eb4e856129
Daniel Schneider <dschneider0103@gmail.com>
2025-03-17 17:47:39 +0100

    Simplify statusline

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,50 +1,23 @@
--- Define mode map (for active windows)
-local mode_map = {
-  n = "NORMAL",
-  i = "INSERT",
-  v = "VISUAL",
-  V = "V-LINE",
-  [""] = "V-BLOCK",
-  c = "COMMAND",
-  R = "REPLACE",
-  t = "TERMINAL",
-}
-
-function CurrentMode()
-  local mode = vim.api.nvim_get_mode().mode
-  return mode_map[mode] or "UNKNOWN"
-end
-
-function Cwd()
-  local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ":~") .. "/"
-  local parts = vim.split(cwd, "/")
-
-  if #parts > 4 then
-    return table.concat({ "...", parts[#parts - 2], parts[#parts - 1], "" }, "/")
-  end
-  return cwd
-end
-
-function Gitbranch()
-  local branch = vim.fn.systemlist("git branch --show-current 2> /dev/null")[1]
-  return branch or ""
-end
-
 -- Active statusline (full details)
 local active_statusline = table.concat({
-  " %{%v:lua.CurrentMode()%}",              -- Mode 
-  " | ",                                    -- Separator
-  -- "%= ",                                    -- Right align
-  -- "%#StatusLineCWD#%{%v:lua.Cwd()%}%*",     -- StatusLineCWD
-  "%f",                                     -- File
-  " | ",                                    -- Separator
-  " %{%v:lua.Gitbranch()%} ",              -- Branch
-  "%= ",                                    -- Right align
-  "[%l:%c] ",                               -- Linenr and column
+  " %{toupper(mode())}",
+  " | ",
+  "%f",
+  " | ",
+  " %{FugitiveHead()} ",
+  "%= ",
+  "[%l:%c] ",
 })
 
 -- Inactive statusline (simplified)
-local inactive_statusline = "%=%#Statement#%f%*%="
+local inactive_statusline = table.concat({
+  "%=",
+  "%#Statement#",
+  "%t",
+  "%*",
+  "%=",
+})
+
 vim.opt.statusline = inactive_statusline
 vim.wo.statusline = active_statusline
 
