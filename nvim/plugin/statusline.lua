3c69ac6009389b10e780e004868256accc31c6f9
Daniel Schneider <dschneider0103@gmail.com>
2025-08-13 15:00:14 +0200

    hello

---
+++ nvim/plugin/statusline.lua
@@ -0,0 +1,30 @@
+function workspaces()
+  local line = {}
+  local currenttab = vim.fn.tabpagenr()
+
+  table.insert(line, "%=")
+  for tab=1, vim.fn.tabpagenr("$") do
+    local cwd = vim.fn.fnamemodify(vim.fn.getcwd(0, tab), ":~")
+    table.insert(line, currenttab == tab and "%#TabLineSel#" or "%#TabLine#")
+    table.insert(line, (vim.t[tab].tabTitle and vim.t[tab].tabTitle or cwd))
+    table.insert(line, " ")
+  end
+  table.insert(line, "%#TabLineFill#")
+  table.insert(line, "%=")
+
+  return table.concat(line)
+end
+
+local function tab_rename()
+  local function on_confirm(input)
+    if not input then return end
+      vim.t.tabTitle = input ~= "" and input or nil
+    end
+  vim.ui.input({prompt = "New tabname (empty for cwd): "}, on_confirm)
+  vim.cmd.redrawtabline()
+end
+
+vim.keymap.set("n", "<leader>,", tab_rename)
+
+vim.opt.tabline = ""
+vim.opt.statusline = "%{%v:lua.workspaces()%}"
